63483a3e1fb5f3e9d0ded906a0f4fffb7edc29e1
Tom <Tom@woodenwindows.com>
2025-08-22 18:59:27 +0000

    some changes/tests

---
+++ components/i2c_dimmer/i2c_dimmer.py
@@ -0,0 +1,26 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import light
+from esphome.const import CONF_OUTPUT_ID
+from esphome import pins
+
+DEPENDENCIES = ['i2c']
+
+CONF_I2C_ADDRESS = 'address'
+CONF_CHANNEL = 'channel'
+
+i2c_dimmer_ns = cg.esphome_ns.namespace('i2c_dimmer')
+I2CDimmerOutput = i2c_dimmer_ns.class_('I2CDimmerOutput', light.LightOutput, cg.Component)
+
+CONFIG_SCHEMA = light.BRIGHTNESS_ONLY_LIGHT_SCHEMA.extend({
+    cv.GenerateID(CONF_OUTPUT_ID): cv.declare_id(I2CDimmerOutput),
+    cv.Required(CONF_I2C_ADDRESS): pins.i2c_device_address,
+    cv.Required(CONF_CHANNEL): cv.uint8_t,
+}).extend(cv.COMPONENT_SCHEMA)
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_OUTPUT_ID])
+    await cg.register_component(var, config)
+    await light.register_light(var, config)
+    cg.add(var.set_i2c_address(config[CONF_I2C_ADDRESS]))
+    cg.add(var.set_channel(config[CONF_CHANNEL]))
