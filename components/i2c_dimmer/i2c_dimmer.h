e040cacf4e84277cc71a77bea3f9323b2637527e
Tom <Tom@woodenwindows.com>
2026-07-03 13:10:04 +0000

    Refactor I2CDimmerOutput to implement I2CDevice interface and update I2C handling in setup and write_state methods

--- components/i2c_dimmer/i2c_dimmer.h
+++ components/i2c_dimmer/i2c_dimmer.h
@@ -1,21 +1,25 @@
+#pragma once
+
 #include "esphome/core/component.h"
+#include "esphome/core/log.h"
+#include "esphome/components/i2c/i2c.h"
 #include "esphome/components/light/light_output.h"
 #include "esphome/components/light/light_traits.h"
 #include "esphome/components/light/light_state.h"
-#include <Wire.h>
 
 namespace esphome {
 namespace i2c_dimmer {
 
-class I2CDimmerOutput : public Component, public light::LightOutput {
+static const char *const TAG = "I2CDimmer";
+
+class I2CDimmerOutput : public Component, public light::LightOutput, public i2c::I2CDevice {
  public:
-  void set_i2c_address(uint8_t i2c_address) { this->i2c_address_ = i2c_address; }
   void set_channel(uint8_t channel) { this->channel_ = channel; }
 
   void setup() override {
-    Wire.beginTransmission(this->i2c_address_);
-    if (Wire.endTransmission() != 0) {
-      ESP_LOGW("I2CDimmer", "Failed to initialize I2C device at address 0x%02X", this->i2c_address_);
+    // Zero-length write acts as a probe, same as Wire.beginTransmission/endTransmission did
+    if (this->write(nullptr, 0) != i2c::ERROR_OK) {
+      ESP_LOGW(TAG, "Failed to initialize I2C device at address 0x%02X", this->address_);
     }
   }
 
@@ -28,20 +32,18 @@ class I2CDimmerOutput : public Component, public light::LightOutput {
   void write_state(light::LightState *state) override {
     uint8_t brightness = 0;
     if (state->current_values.get_state()) {
-      brightness = (uint8_t)(state->current_values.get_brightness() * 100);
+      brightness = (uint8_t) (state->current_values.get_brightness() * 100);
     }
-    ESP_LOGD("I2CDimmer", "Writing brightness %d to address 0x%02X, channel 0x%02X", brightness, this->i2c_address_, this->channel_);
-    Wire.beginTransmission(this->i2c_address_);
-    Wire.write(this->channel_);
-    Wire.write(brightness);
-    if (Wire.endTransmission() != 0) {
-      ESP_LOGE("I2CDimmer", "Failed to write to I2C device at address 0x%02X, channel 0x%02X", this->i2c_address_, this->channel_);
+    ESP_LOGD(TAG, "Writing brightness %d to address 0x%02X, channel 0x%02X", brightness, this->address_,
+             this->channel_);
+    uint8_t data[2] = {this->channel_, brightness};
+    if (this->write(data, sizeof(data)) != i2c::ERROR_OK) {
+      ESP_LOGE(TAG, "Failed to write to I2C device at address 0x%02X, channel 0x%02X", this->address_,
+               this->channel_);
     }
-    // delayMicroseconds(500);  // Experimental: Small delay to space I2C commands during rapid transitions (adjust or remove if issues)
   }
 
  protected:
-  uint8_t i2c_address_;
   uint8_t channel_;
 };
 
--- components/i2c_dimmer/light.py
+++ components/i2c_dimmer/light.py
@@ -1,26 +1,32 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import light
+from esphome.components import i2c, light
 from esphome.const import CONF_OUTPUT_ID
-from esphome import pins
 
 DEPENDENCIES = ['i2c']
 
-CONF_I2C_ADDRESS = 'address'
 CONF_CHANNEL = 'channel'
 
 i2c_dimmer_ns = cg.esphome_ns.namespace('i2c_dimmer')
-I2CDimmerOutput = i2c_dimmer_ns.class_('I2CDimmerOutput', light.LightOutput, cg.Component)
+I2CDimmerOutput = i2c_dimmer_ns.class_(
+    'I2CDimmerOutput', light.LightOutput, cg.Component, i2c.I2CDevice
+)
 
-CONFIG_SCHEMA = light.BRIGHTNESS_ONLY_LIGHT_SCHEMA.extend({
-    cv.GenerateID(CONF_OUTPUT_ID): cv.declare_id(I2CDimmerOutput),
-    cv.Required(CONF_I2C_ADDRESS): cv.i2c_address,
-    cv.Required(CONF_CHANNEL): cv.uint8_t,
-}).extend(cv.COMPONENT_SCHEMA)
+CONFIG_SCHEMA = (
+    light.BRIGHTNESS_ONLY_LIGHT_SCHEMA.extend(
+        {
+            cv.GenerateID(CONF_OUTPUT_ID): cv.declare_id(I2CDimmerOutput),
+            cv.Required(CONF_CHANNEL): cv.uint8_t,
+        }
+    )
+    .extend(cv.COMPONENT_SCHEMA)
+    .extend(i2c.i2c_device_schema(None))
+)
+
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_OUTPUT_ID])
     await cg.register_component(var, config)
     await light.register_light(var, config)
-    cg.add(var.set_i2c_address(config[CONF_I2C_ADDRESS]))
+    await i2c.register_i2c_device(var, config)
     cg.add(var.set_channel(config[CONF_CHANNEL]))
